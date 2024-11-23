f92830bc36f7e8eafc61b56da1823339a8334ea1
jobeda <jobedakhatun710@gmail.com>
2024-11-23 19:10:27 +0600

    display blog data

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,15 +1,34 @@
 import PropTypes from 'prop-types';
  const Blog = ({blog}) => {
   console.log(blog)
+  const {title ,cover_image ,author_name,author_img,created_date,reading_time,hashtag,description}=blog;
  return (
   <div>
-   
+      <img src={cover_image} alt="{`not found ${cover_image}`}" />
+      <div className='flex justify-between'>
+       <div className='flex '>
+            <img className='w-[60px] h-[60px] rounded-full' src={author_img} alt="" />
+            <div className='ml-6'>
+               <h3 className='text-2xl'>{author_name}</h3>
+               <p>{created_date}</p>
+            </div>
+            
+       </div>
+       <div>
+        <span>{reading_time}</span>
+       </div>
+
+      </div>
+      <h3 className="text-4xl">{title}</h3>
+      <p>
+       {hashtag.map((hash ,idx)=><span key={idx}><a href='#'>{hash}</a></span>)}
+      </p>
   </div>
  );
 };
 
 Blog.PropTypes ={
-  blog:PropTypes.objects.isRequired
+  blog:PropTypes.object.isRequired
 }
 
 export default Blog;
