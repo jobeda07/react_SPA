a345c912816f935d387363c6d80cff28e7447f94
jobeda <jobedakhatun710@gmail.com>
2024-11-23 18:47:49 +0600

    set layout and props add

--- src/components/Blogs/Blog.jsx
+++ src/components/Blogs/Blog.jsx
@@ -1,9 +1,15 @@
- const Blog = () => {
+import PropTypes from 'prop-types';
+ const Blog = ({blog}) => {
+  console.log(blog)
  return (
   <div>
    
   </div>
  );
 };
+
+Blog.PropTypes ={
+  blog:PropTypes.objects.isRequired
+}
 
 export default Blog;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 
 const Header = () => {
  return (
-  <div>
-           <h1 className="text-3xl font-bold"> Hello Picchi!</h1>
-  </div>
+  <header className="flex justify-between items-center p-4 mx-4">
+      <h1 className="text-3xl font-bold"> Hello Picchi!</h1>
+      <img className="w-[60px] h-[60px] rounded-full" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+  </header>
  );
 };
 
